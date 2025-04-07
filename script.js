73459338db7df4e6bd8c1d0657c8955166f4be70
fedoroleg <fedoroleg@gmail.com>
2025-04-07 08:38:44 +0300

    add theme toggle

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-console.log("works!");
+const darkModeToggle = document.querySelector(".dark-mode-toggle");
 
-// ждем полной загрузки страницы
-window.onload = () => {
-  console.log("onload run!");
+if (localStorage.getItem("darkMode") === "dark") {
+  darkModeToggle.classList.add("dark-mode-toggle-active");
+  document.documentElement.dataset.theme = "dark";
+}
 
-  const imgObserver = new IntersectionObserver(
-    (entries, observer) => {
-      console.log("entries: ", entries);
+darkModeToggle.addEventListener("click", function () {
+  const isDark = darkModeToggle.classList.toggle("dark-mode-toggle-active");
 
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.src = entry.target.dataset.source;
-
-          observer.unobserve(entry.target);
-        }
-      });
-    },
-    { threshold: 0.5 }
-  );
-  document.querySelectorAll("img").forEach((img) => imgObserver.observe(img));
-};
+  if (isDark) {
+    localStorage.setItem("darkMode", "dark");
+    document.documentElement.dataset.theme = "dark";
+  } else {
+    localStorage.setItem("darkMode", "light");
+    document.documentElement.dataset.theme = "light";
+  }
+});
